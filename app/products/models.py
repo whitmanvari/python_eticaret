afd3ebe5e87ae1a15b4da35cd0d56b16ada12b98
İraz Hazal İlik <ilikhazal16@gmail.com>
2026-06-11 09:44:47 +0300

    Product , Categories, Orders, Users gibi modelleri app altında başlattım. Bunları settings.py'de tanıttım.

---
+++ app/products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=200) #karakter tabanlı bir veri alanı yansıyacak veritabanına.
+    description= models.TextField(blank=True) #boş bir değer alabilir.
+    price= models.DecimalField(max_digits=10, decimal_places=2) #bu decimal fiyat alanının max karakteri 10 basamaklı olsun, virgünden sonraki kısım 2 basamaklı olsun.
+    stock = models.PositiveIntegerField(default=0)
+
+    #veriyi temsil etme--> ürünlerin isimleri gelsin
+    def __str__(self):
+        return self.name
