14e763bed7c84bcae07c857ee7e7e25b51983b67
İraz Hazal İlik <ilikhazal16@gmail.com>
2026-06-13 12:55:11 +0300

    id'ye göre ürün detayı getiren product_details endpoint'i ekledim, <int:pk> rotası ile tekil ürün filtreleme eklemiş oldum.

--- app/products/urls.py
+++ app/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import product_list
+from .views import product_list, product_details
 #app'le ilgili url'leri alacağımız alan. 
 urlpatterns = [
-    path('list/', product_list, name='products')
+    path('list/', product_list, name='products'),
+    path('<int:pk>', product_details, name='product_details'),
 
 ]
--- app/products/views.py
+++ app/products/views.py
@@ -10,4 +10,12 @@ def product_list(request):
     }
     return JsonResponse(data) #queryset direkt jsona çevrilmediği için yaptık.
 
-
+def product_details(request, pk):
+    product = Product.objects.get(pk=pk)
+    data= {
+        'name': product.name,
+        'description': product.description,
+        'price': product.price,
+        'stock': product.stock
+    }
+    return JsonResponse(data)
