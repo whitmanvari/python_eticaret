49825bf370a5a5a7beec22bc07873f1bdd4dc72e
İraz Hazal İlik <ilikhazal16@gmail.com>
2026-06-14 00:01:52 +0300

    status codeları ayarladım ve rest_framework'ün status codeları olan HTTP_..._.. hata mesajlarını responselarıma ekledim.

--- app/products/views.py
+++ app/products/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, get_object_or_404
 from .models import Product
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from .serializers import ProductSerializer
-from rest_framework.response import Response  
+from rest_framework.response import Response
+from rest_framework import status  
 
 #default get requesti bunlar. 
 @api_view(['GET', 'POST'])
@@ -12,22 +11,29 @@ def product_list(request):
     if request.method== 'GET':
         products= Product.objects.all()
         serializer = ProductSerializer(products, many=True) #products liste olarak geliyor
-        return Response(serializer.data)
+        return Response(serializer.data) #200 ok
+    
     if request.method=='POST':
         serializer= ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED) #Bir veri karşı tarafa yüklendi, post oldu 201.  
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #post hatası varsa status 400 bad request.
 
    
 @api_view(['GET', 'PUT'])
 def product_details(request, pk):
+
     if request.method == 'GET':
-        product= Product.objects.get(pk=pk)
+        try:
+            product= Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response('Error: Product not found.', status=status.HTTP_404_NOT_FOUND) # 404 not found yani aradığın id yok diyebiliriz.
+     
         serializer = ProductSerializer(product) #tekil bir bilgi alıyoruz
         return Response(serializer.data)
+    
     if request.method == 'PUT':
         product= Product.objects.get(pk=pk)
         serializer = ProductSerializer(product,data=request.data)
@@ -35,7 +41,7 @@ def product_details(request, pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #post hatası varsa status 400 bad request.
 
 
 #SERILIZATION
@@ -62,3 +68,4 @@ def product_details(request, pk):
     return JsonResponse(data)
     
     bu manuel yöntemdi.."""
+
