d293a57cab1ec5cab35ac21e4950c649f1a84d0d
İraz Hazal İlik <ilikhazal16@gmail.com>
2026-06-14 00:18:18 +0300

    Categories için bir model oluşturdum. Products modeli içerisine category ve slug proplarını ekledim. Yeniden migration attım. Category propu olarak name ve description verdim. Admin.py içerisinde de site.register diyerek bu modelimi yolladım.

--- app/products/models.py
+++ app/products/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from categories.models import Category
 
 class Product(models.Model):
     name = models.CharField(max_length=200) #karakter tabanlı bir veri alanı yansıyacak veritabanına.
     description= models.TextField(blank=True) #boş bir değer alabilir.
     price= models.DecimalField(max_digits=10, decimal_places=2) #bu decimal fiyat alanının max karakteri 10 basamaklı olsun, virgünden sonraki kısım 2 basamaklı olsun.
     stock = models.PositiveIntegerField(default=0)
-
+    slug= models.SlugField(default='')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', default=1)
     #veriyi temsil etme--> ürünlerin isimleri gelsin
     def __str__(self):
         return self.name
