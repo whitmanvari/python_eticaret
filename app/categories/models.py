fbd5764652be9542279f6d9d8d3cfd74b83fec71
İraz Hazal İlik <ilikhazal16@gmail.com>
2026-06-14 09:39:44 +0300

    Category validasyon kuralları ekledim. Name ve description alanları aynı olamaz. Name max length 100 olacak modeldeki gibi. İsim eğer 2 karakterden az olursa hata verecek şeklinde validasyonlar yazdım serializers içerisinde.

--- app/categories/models.py
+++ app/categories/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name =models.CharField(max_length=100,  unique=True)
-    description = models.TextField(blank=True)
+    #null=true diyerek dbnin boş bir değer kabul etmesini etkiliyoruz, blank dediğimizde formdan boş değer gelmeini.
+    description = models.TextField(blank=True, null=True)
     
     def __str__(self):
         return self.name
--- app/categories/serializers.py
+++ app/categories/serializers.py
@@ -1,18 +1,31 @@
 from rest_framework import serializers
 from .models import Category
+from rest_framework.validators import UniqueValidator
 
 class CategorySerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name= serializers.CharField()
-    description = serializers.CharField()
+    name= serializers.CharField(max_length=100,validators=[UniqueValidator(queryset=Category.objects.all())])
+    #allow null--> {"name": "Kozmetik", "description": null}
+    #allow_blank --> {"name": "Kozmetik", "description": ""}
+    #required=False --> {"name": "Kozmetik"} şeklinde gönderebilmeye yarar.
+    description = serializers.CharField(required=False, allow_blank=True, allow_null=True)
 
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Name and Description should be different. ")
+        return data
     #post
     def create(self, validated_data):
         return Category.objects.create(**validated_data)
     
     #put
     def update(self, instance, validated_data):
-        instance.name=self.validated_data.get('name', instance.name)
-        instance.description=self.validated_data.get('description', instance.description)
+        instance.name=validated_data.get('name', instance.name)
+        instance.description=validated_data.get('description', instance.description)
         instance.save()
         return instance
+    
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("You must enter at least 2 character.")
+        return value
