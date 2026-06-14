7632c7da455be406f7ff514976d8afae3edf72cb
İraz Hazal İlik <ilikhazal16@gmail.com>
2026-06-14 23:15:24 +0300

    Comment modeli oluşturdum. Yeniden migration attım ve setting.py içerisine ve admin içerisine comment modelini yolladım.

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'orders',
     'categories',
     'users',
+    'comments',
     'rest_framework'
 ]
 
---
+++ app/comments/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from products.models import Product
+
+class Comment(models.Model):
+    rating= models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=200, null=True)
+    active=models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    product= models.ForeignKey(Product, on_delete=models.CASCADE,related_name="comments" )
+
+    def __str__(self):
+        return str(self.rating) + " | " + self.product.name
