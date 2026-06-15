9fee741db04b7854d2ce8ff69c5acc78a793ab54
İraz Hazal İlik <ilikhazal16@gmail.com>
2026-06-15 12:32:57 +0300

    ListAPIView ile çoklu veri, RetrieveAPIView ile tekil veri çekerek endpointler oluşturdum. Comment için artık comments/2/product dediğimizde 2 ID'li producta yapılan yorumları, comments/2 dediğimizde 2 ID'li yorumu (tekil bir biçimde) getirecek.

--- app/comments/urls.py
+++ app/comments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CommentListView
+from .views import CommentListView, CommentListByProductView, CommentDetailsView
 
 urlpatterns = [
-    path('', CommentListView.as_view(), name='comments')]
-
-
+    path('', CommentListView.as_view(), name='comments'),
+    path('<int:pk>/product', CommentListByProductView.as_view(), name='comments_by_product'),
+    path('<int:pk>', CommentDetailsView.as_view(), name='comments_details')
+]
--- app/comments/views.py
+++ app/comments/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
+#from rest_framework.views import APIView
 from rest_framework import generics
 from .models import Comment
 from .serializers import CommentSerializer
-from rest_framework import mixins
+#from rest_framework import mixins
 
-#ListAPIView(mixins.ListModelMixin.GenericAPIView)---> list api view aslında mixins'in Listmodelmixin'inin generic api view'indne türemiştir. Mixin kullanmadan direkt yazılabiliyor. 
+#ListAPIView(mixins.ListModelMixin.GenericAPIView)---> list api view aslında mixins'in Listmodelmixin'inin generic api view'indne türemiştir. Mixin kullanmadan direkt yazılabiliyor.
 class CommentListView(generics.ListAPIView):
     queryset=Comment.objects.all()
     serializer_class= CommentSerializer
 
-    #*args (Positional arguments / Konumsal argümanlar) ve kwargs (Keyword arguments / İsimli argümanlar), Python'da bir fonksiyona veya metoda esnek sayıda argüman gönderilmesini sağlayan yapılardır.
+    """#*args (Positional arguments / Konumsal argümanlar) ve kwargs (Keyword arguments / İsimli argümanlar), Python'da bir fonksiyona veya metoda esnek sayıda argüman gönderilmesini sağlayan yapılardır.
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
-    #çift yıldız dictionarye koy, tek yıldız tuple olarak al 
+    #çift yıldız dictionarye koy, tek yıldız tuple olarak al """
 
+class CommentListByProductView(generics.ListAPIView):
+    serializer_class= CommentSerializer
+
+    def get_queryset(self):
+        pk=self.kwargs['pk']
+        return Comment.objects.filter(product_id=pk)
     
+class CommentDetailsView(generics.RetrieveAPIView):
+    queryset=Comment.objects.all()
+    serializer_class= CommentSerializer
+
+"""ListAPIView---> veritabanındaki birden fazla objeyi listelemek için kullanılır. 
+Yaptığı: Veritabanından queryset'i çeker ve bunu serializer ile dönüştürür ve bir liste formatında döndürür. 
+
+RetrieveAPIView(tekil veri)---> veritabından tek bir objeyi getirmek için kullanılır. 
+Yaptığı: URL'den gelen pk veya slug gibi ID'yi kullanır. veritabanında sadece o ID' ye ait olan tek bir kaydı bulur ve 
+tek bir obje (dictionary) olarak döner.
+
+
+ListAPIView= GenericAPIView + ListModelMixin (listeleme yeteneği ekler)
+RetrieveAPIView=GenericAPIView + RetrieveModelMixin (tekil veri öekme yeteneği ekler)"""
