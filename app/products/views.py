78696a4f088e3ab31723cca527f827841adea26b
İraz Hazal İlik <ilikhazal16@gmail.com>
2026-06-17 16:14:45 +0300

    Product ile ilgili endpointlerimi refactor işlemim bitti, gerekli bilgileri ve postman testlerimi de yaptım.

--- app/products/views.py
+++ app/products/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status  
 from rest_framework.permissions import IsAdminUser
 
+#Needs permission, admin lists all products
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
 def admin_list_products(request):
@@ -12,7 +13,8 @@ def admin_list_products(request):
     products= Product.objects.all()
     serializer = ProductListSerializer(products, many=True) #products liste olarak geliyor
     return Response(serializer.data) #200 ok
-    
+
+#No permission need, list all products
 @api_view(['GET'])
 def catalog_list_products(request):
     """Catalog: List all products"""
@@ -20,6 +22,7 @@ def catalog_list_products(request):
     serializer = ProductListSerializer(products, many=True) 
     return Response(serializer.data) 
 
+#No permission need, get product details by ID
 @api_view(['GET'])
 def catalog_product_details(request, pk):
 
@@ -31,6 +34,7 @@ def catalog_product_details(request, pk):
     serializer = ProductDetailsSerializer(product) 
     return Response(serializer.data)
     
+#permission needed, get product details by ID
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
 def admin_product_details(request, pk):
@@ -43,7 +47,7 @@ def admin_product_details(request, pk):
     serializer = ProductDetailsSerializer(product) 
     return Response(serializer.data)
 
-#admin_create_product
+#Permission needed, post a new product 
 @api_view(['POST'])
 def admin_create_product(request):
     serializer= ProductSerializer(data=request.data)
@@ -54,23 +58,7 @@ def admin_create_product(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #post hatası varsa status 400 bad request.
         
 
-#default get requesti bunlar. 
-@api_view(['GET', 'POST'])
-def product_list(request):
-
-    if request.method== 'GET':
-        products= Product.objects.all()
-        serializer = ProductSerializer(products, many=True) #products liste olarak geliyor
-        return Response(serializer.data) #200 ok
-    
-    if request.method=='POST':
-        serializer= ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED) #Bir veri karşı tarafa yüklendi, post oldu 201.  
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #post hatası varsa status 400 bad request.
-
+#Permission needed, edit products by ID 
 @api_view(['PUT'])
 @permission_classes([IsAdminUser])
 def admin_edit_product(request, pk):
@@ -83,28 +71,7 @@ def admin_edit_product(request, pk):
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
-@api_view(['GET', 'PUT'])
-def product_details(request, pk):
-
-    if request.method == 'GET':
-        try:
-            product= Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            return Response('Error: Product not found.', status=status.HTTP_404_NOT_FOUND) # 404 not found yani aradığın id yok diyebiliriz.
-     
-        serializer = ProductSerializer(product) #tekil bir bilgi alıyoruz
-        return Response(serializer.data)
-    
-    if request.method == 'PUT':
-        product= Product.objects.get(pk=pk)
-        serializer = ProductSerializer(product,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #post hatası varsa status 400 bad request.
-
- 
+#Permission needed, delete product by ID 
 @api_view(['DELETE'])
 @permission_classes([IsAdminUser])
 def admin_delete_product(request, pk):
