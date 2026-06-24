b04639c7140666c2fd77f45e3f8a130636fb1900
İraz Hazal İlik <ilikhazal16@gmail.com>
2026-06-24 13:13:42 +0300

    jwt token'ı programın setting.pysine tanıttım ve users sınıfımdaki urls.py içerisine path olarak ekledim.

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,6 +127,7 @@ AUTH_USER_MODEL= "users.CustomUser"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
--- app/users/urls.py
+++ app/users/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import SignUpView, LoginView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path('signup', SignUpView.as_view(), name='signup'),
-    path('login', LoginView.as_view(), name='login')
+    path('login', LoginView.as_view(), name='login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
