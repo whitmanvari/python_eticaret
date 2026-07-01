d887cfc3ad8698d95b4c0e91028478b34cfc19bc
İraz Hazal İlik <ilikhazal16@gmail.com>
2026-07-02 00:36:26 +0300

    item_id olan ufak yazım hatasını düzeltiyorum. pk olarak endpointe uygun bir biçimde düzeltiyorum.

--- app/carts/urls.py
+++ app/carts/urls.py
@@ -4,6 +4,6 @@ from .views import AddToCartView, CartDetailView, UpdateCartItemView
 urlpatterns = [
     path('', CartDetailView.as_view(), name='cart_detail'),
     path('add/', AddToCartView.as_view(), name='cart_add'),
-    path('update/<int:item_id>/', UpdateCartItemView.as_view(), name='cart_item_update'),
-    # path('delete/<int:item_id>/', DeleteCartItemView.as_view(), name='delete_cart_item'),
+    path('update/<int:pk>/', UpdateCartItemView.as_view(), name='cart_item_update'),
+    # path('delete/<int:pk>/', DeleteCartItemView.as_view(), name='delete_cart_item'),
 ]
